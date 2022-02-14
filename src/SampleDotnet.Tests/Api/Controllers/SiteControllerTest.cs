56d15756e386f2ecc8b11a4dac964af1ca78d52a
Roberto Ruiz <roberto.ruiz01@globant.com>
2022-02-14 18:42:18 -0300

    Add empty projects and a sample test

---
+++ src/SampleDotnet.Tests/Api/Controllers/SiteControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SampleDotnet.Api.Controllers;
+using SampleDotnet.Application.Query.Site.GetAll;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SampleDotnet.Tests.Api.Controllers
+{
+    public class SiteControllerTest
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+
+        private readonly SiteController _sut;
+
+        public SiteControllerTest()
+        {
+            _mediatorMock = new Mock<IMediator>();
+
+            _sut = new SiteController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_Success()
+        {
+            _mediatorMock.Setup(x => x.Send(It.IsAny<GetAllSitesRequest>(), CancellationToken.None))
+                .ReturnsAsync(new GetAllSitesResponse());
+
+            var _ = await _sut.GetAll();
+
+            Assert.IsAssignableFrom<OkObjectResult>(_);
+            Assert.IsAssignableFrom<GetAllSitesResponse>(((OkObjectResult)_).Value);
+            _mediatorMock.Verify(x => x.Send(It.IsAny<GetAllSitesRequest>(), CancellationToken.None), Times.Once);
+        }
+    }
+}
